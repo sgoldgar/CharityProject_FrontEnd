7ff186850aca630d046370754c6d402a103b7359
Sarah Goldgar <sgoldgar@gmail.com>
2017-05-16 13:39:45 -0500

    Update donorProfile.js

--- js/donorProfile.js
+++ js/donorProfile.js
@@ -3,7 +3,7 @@ console.log('all ready');
 
 $(function() {
     console.log( "test!");
-    var databaseUrl= 'https://sgoldgar.github.io/CharityProject_FrontEnd/charity_portal';
+    var databaseUrl= 'https://secret-peak-23167.herokuapp.com/';
     var getNeeds = $.ajax({
     url: databaseUrl,
     image:
